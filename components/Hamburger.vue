73a8781aac42c010eade7e668818769d5d34bdb9
Pablo Bozzolo <boctulus@gmail.com>
2020-02-05 23:39:28 -0300

    .

---
+++ components/Hamburger.vue
@@ -0,0 +1,81 @@
+<template>
+    <a id="hamburger-icon" href="#" title="Menu">
+        <span class="line line-1"></span>
+        <span class="line line-2"></span>
+        <span class="line line-3"></span>
+    </a>
+</template>
+
+<script>
+export default {
+     name: 'Hamburger',
+}
+</script>
+
+<style scoped>
+@import url(https://fonts.googleapis.com/css?family=Montserrat:400,700);
+
+ #hamburger-icon {
+	 width: 40px;
+	 height: 20px;
+	 position: relative;
+	 display: block;
+	 margin: 40px auto 20px auto;
+}
+ #hamburger-icon .line {
+	 display: block;
+	 background: #B38C25;
+	 width: 40px;
+	 height: 4px;
+	 position: absolute;
+	 left: 0;
+	 border-radius: 2px;
+	 transition: all 0.4s;
+	 -webkit-transition: all 0.4s;
+	 -moz-transition: all 0.4s;
+}
+ #hamburger-icon .line.line-1 {
+	 top: 0;
+}
+ #hamburger-icon .line.line-2 {
+	 top: 50%;
+}
+ #hamburger-icon .line.line-3 {
+	 top: 100%;
+}
+ #hamburger-icon:hover .line-1, #hamburger-icon:focus .line-1 {
+	 transform: translateY(-2px);
+	 -webkit-transform: translateY(-2px);
+	 -moz-transform: translateY(-2px);
+}
+ #hamburger-icon:hover .line-3, #hamburger-icon:focus .line-3 {
+	 transform: translateY(2px);
+	 -webkit-transform: translateY(2px);
+	 -moz-transform: translateY(2px);
+}
+ #hamburger-icon.active .line-1 {
+	 transform: translateY(10px) translateX(0) rotate(45deg);
+	 -webkit-transform: translateY(10px) translateX(0) rotate(45deg);
+	 -moz-transform: translateY(10px) translateX(0) rotate(45deg);
+}
+ #hamburger-icon.active .line-2 {
+	 opacity: 0;
+}
+ #hamburger-icon.active .line-3 {
+	 transform: translateY(-10px) translateX(0) rotate(-45deg);
+	 -webkit-transform: translateY(-10px) translateX(0) rotate(-45deg);
+	 -moz-transform: translateY(-10px) translateX(0) rotate(-45deg);
+}
+ h1 {
+	 text-transform: uppercase;
+}
+ a {
+	 text-decoration: none;
+	 color: #95a5a6;
+	 margin: 0.5em 1.5em;
+	 display: inline-block;
+}
+ a:hover, a:focus {
+	 color: #ecf0f1;
+} 
+</style>
--- components/RocketNavigation.vue
+++ components/RocketNavigation.vue
@@ -2,125 +2,65 @@
     <span>
         <v-navigation-drawer app v-model="drawer" right>
             <v-list>
-                <template v-for="(menu, i) in menus">
+                <template v-for="(list, i) in lists">
                     <v-list-tile :key="i">
                         <v-list-tile-content>
-                            {{menu.value}}
+                            <transition name="component-fade" mode="out-in">
+                                <span v-if="drawer">{{list.msg}}</span>
+                            </transition>
                         </v-list-tile-content>
                     </v-list-tile>
                     <v-divider :key="`divider-${i}`"></v-divider>
                 </template>
-            </v-list>
+            </v-list>      
         </v-navigation-drawer>
 
         <nav style="display:flex; align-items: flex-start; margin: 40px">
             <v-img :src="require('../assets/logo.png')" :max-width="80"></v-img>
 
             <v-divider></v-divider>
-            
-            <a id="hamburger-icon" href="#" title="Menu" @click="drawer = !drawer" v-bind:class="[drawer ? 'active' : '']" style="z-index: 9999;">
-                <span class="line line-1"></span>
-                <span class="line line-2"></span>
-                <span class="line line-3"></span>
-            </a>
+
+            <Hamburger @click.native="drawer = !drawer" v-bind:class="[drawer ? 'active' : '']" style="z-index: 9999;"></Hamburger>            
         </nav>
-
-        
-
     </span>
 </template>
 
 <script>
+import Hamburger from '@/components/Hamburger';
+import Velocity from 'velocity-animate';
+
 export default {
     name: 'RocketNavigation',
     data() {
         return {
             drawer: false,
-            menus: [
-                { value: 'Pricing' },
-                { value: 'Customers' },
-                { value: 'Docs' },
-                { value: 'Blog' },
-                { value: 'Login' },
-                { value: 'Sign Up' }
+            lists: [
+                { msg: 'Pricing' },
+                { msg: 'Customers' },
+                { msg: 'Docs' },
+                { msg: 'Blog' },
+                { msg: 'Login' },
+                { msg: 'Sign Up' }
             ]
         };
     },
     components: {
+        Hamburger
     }
 };
 </script>
 
+
 <style scoped>
-@import url(https://fonts.googleapis.com/css?family=Montserrat:400,700);
- body {
-	 background: #3d566e;
-	 color: #ecf0f1;
-	 font-family: 'Montserrat', sans-serif;
-	 -webkit-font-smoothing: antialiased;
-	 text-align: center;
+.component-fade-enter-active {
+  transition: opacity 4s ease;
 }
- #hamburger-icon {
-	 width: 40px;
-	 height: 20px;
-	 position: relative;
-	 display: block;
-	 margin: 40px auto 20px auto;
+
+.component-fade-leave-active {
+  transition: opacity 4s ease;
 }
- #hamburger-icon .line {
-	 display: block;
-	 background: #B38C25;
-	 width: 40px;
-	 height: 4px;
-	 position: absolute;
-	 left: 0;
-	 border-radius: 2px;
-	 transition: all 0.4s;
-	 -webkit-transition: all 0.4s;
-	 -moz-transition: all 0.4s;
+
+.component-fade-enter, .component-fade-leave-to {
+  opacity: 0;
 }
- #hamburger-icon .line.line-1 {
-	 top: 0;
-}
- #hamburger-icon .line.line-2 {
-	 top: 50%;
-}
- #hamburger-icon .line.line-3 {
-	 top: 100%;
-}
- #hamburger-icon:hover .line-1, #hamburger-icon:focus .line-1 {
-	 transform: translateY(-2px);
-	 -webkit-transform: translateY(-2px);
-	 -moz-transform: translateY(-2px);
-}
- #hamburger-icon:hover .line-3, #hamburger-icon:focus .line-3 {
-	 transform: translateY(2px);
-	 -webkit-transform: translateY(2px);
-	 -moz-transform: translateY(2px);
-}
- #hamburger-icon.active .line-1 {
-	 transform: translateY(10px) translateX(0) rotate(45deg);
-	 -webkit-transform: translateY(10px) translateX(0) rotate(45deg);
-	 -moz-transform: translateY(10px) translateX(0) rotate(45deg);
-}
- #hamburger-icon.active .line-2 {
-	 opacity: 0;
-}
- #hamburger-icon.active .line-3 {
-	 transform: translateY(-10px) translateX(0) rotate(-45deg);
-	 -webkit-transform: translateY(-10px) translateX(0) rotate(-45deg);
-	 -moz-transform: translateY(-10px) translateX(0) rotate(-45deg);
-}
- h1 {
-	 text-transform: uppercase;
-}
- a {
-	 text-decoration: none;
-	 color: #95a5a6;
-	 margin: 0.5em 1.5em;
-	 display: inline-block;
-}
- a:hover, a:focus {
-	 color: #ecf0f1;
-} 
 </style>
