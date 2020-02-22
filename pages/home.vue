0da1f3eb3701b889e132cb0e6c49f783284c7458
Pablo Bozzolo <boctulus@gmail.com>
2020-02-22 11:49:17 -0300

    .

--- pages/home.vue
+++ pages/home.vue
@@ -104,7 +104,7 @@
 
             <b-row>
             <b-col sm="12" md="6">
-              <v-img   :src="require('../assets/torre_juan.jpg')" lazy-src class="quienes_img" style="margin-bottom: 50px;"   />
+              <v-img   :src="require('../assets/torre_juan2.png')" lazy-src class="quienes_img" style="margin-bottom: 50px;"   />
             </b-col>
 
             <b-col sm="12" md="6"  class="mt-3">  
@@ -128,84 +128,88 @@
         </v-img>      
            
               
-      		<v-img :src="require('../assets/profesionales-certificados-prosoc.png')"    lazy-src class="img img4">
-              <b-container style="color: #ffffff;">        
-                    <b-row class="mb-5 mt-5 mx-auto"><h3 class="contact_form_h">CONTACTO</h3></b-row>  
+        <v-img :src="require('../assets/profesionales-certificados-prosoc.png')"    lazy-src class="img img4">
+          <b-container style="color: #ffffff;">        
+                <b-row class="mb-5 mt-5 mx-auto"><h3 class="contact_form_h">CONTACTO</h3></b-row>  
 
+                <b-row>
+                  <b-col md="6" class="engravers">
+                    <b-row class="mb-3 ml-auto contact_info">Teléfono: (221) 15 545-2109</b-row>
+                    <b-row class="mb-3 ml-auto contact_info">Correo: adm.elgrove@outlook.com</b-row>
+                    <b-row class="mb-3 ml-auto contact_info" style="padding-bottom: 10px;">Dirección: calle 28 nro 167, La Plata, Bs As</b-row>
+                    <b-row class="mb-3 ml-auto contact_info">                      
+                      <img src="https://i.imgur.com/npweaNh.png" alt="" width="20" height="20"/>
+                      <span class="engravers" style="color: #ffffff; padding-left:8px;"><a style="color: white; text-decoration: none; font-size: 20px;" href="/blog" target="_blank" rel="noopener noreferrer">BLOG </a></span>
+                    </b-row>
+                    <b-row class="mx-auto">
+                      <!-- https://www.maps.ie/create-google-map/ -->
+                      <div style="width: 100%"><iframe width="100%" height="276" src="https://maps.google.com/maps?width=100%&amp;height=276&amp;hl=en&amp;q=CALLE%2028%20NRO%20167%2C%20LA%20PLATA%2C%20ARGENTINA+(El%20Grove)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe></div>
+                    </b-row>
+                  </b-col>
+
+                  <b-col md="6"  class="mt-3">
                     <b-row>
-                      <b-col md="6" class="engravers">
-                        <b-row class="mb-3 ml-auto contact_info">Teléfono: (221) 15 545-2109</b-row>
-                        <b-row class="mb-3 ml-auto contact_info">Correo: adm.elgrove@outlook.com</b-row>
-                        <b-row class="mb-3 ml-auto contact_info" style="padding-bottom: 10px;">Dirección: calle 28 nro 167, La Plata, Buenos Aires, Arg.</b-row>
-                        <b-row class="mx-auto">
-                          <!-- https://www.maps.ie/create-google-map/ -->
-                          <div style="width: 100%"><iframe width="100%" height="330" src="https://maps.google.com/maps?width=100%&amp;height=330&amp;hl=en&amp;q=CALLE%2028%20NRO%20167%2C%20LA%20PLATA%2C%20ARGENTINA+(El%20Grove)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe></div>
-                        </b-row>
-                      </b-col>
+                    <b-col md="6" sm="12">
 
-                      <b-col md="6"  class="mt-3">
-                        <b-row>
-                        <b-col md="6" sm="12">
-
-                          <b-form-group
-                          label="NOMBRE"
-                          label-for="input-1"
-                          class="engravers label"
-                          >
-                          <b-form-input id="input-1" trim class="footer-input"></b-form-input>
-                          </b-form-group>   
-
-                        </b-col>
-                        
-                        <b-col md="6" sm="12">
-
-                          <b-form-group
-                          label="TELEFONO"
-                          label-for="input-2"
-                          class="engravers label"
-                          >
-                          <b-form-input id="input-2" trim class="footer-input"></b-form-input>
-                          </b-form-group>   
-
-                        </b-col>
-                        </b-row>    
-                      
-                      
                       <b-form-group
-                        label="EMAIL"
-                        label-for="input-3"
-                        class="engravers label"
+                      label="NOMBRE"
+                      label-for="input-1"
+                      class="engravers label"
                       >
-                        <b-form-input id="input-3" trim class="footer-input"></b-form-input>
+                      <b-form-input id="input-1" trim class="footer-input"></b-form-input>
                       </b-form-group>   
 
-                      <b-form-group
-                        label="SERVICIO"
-                        label-for="input-4"
-                        class="engravers label"
-                      >
-                        <b-form-input id="input-4" trim class="footer-input"></b-form-input>
-                      </b-form-group>  
+                    </b-col>
+                    
+                    <b-col md="6" sm="12">
 
                       <b-form-group
-                        label="CONSULTA"
-                        label-for="input-4"
-                        class="engravers label"
+                      label="TELEFONO"
+                      label-for="input-2"
+                      class="engravers label"
                       >
-                        <b-form-textarea
-                        id="input-5"
-                        placeholder="Su consulta"
-                        rows="3"
-                        max-rows="6"
-                        class="footer-input"
-                        ></b-form-textarea>
-                      </b-form-group>
+                      <b-form-input id="input-2" trim class="footer-input"></b-form-input>
+                      </b-form-group>   
 
-                      <b-button type="submit" block class="footer-input">Enviar</b-button> 
-                      </b-col>
-                    </b-row>                  
+                    </b-col>
+                    </b-row>    
+                  
+                  
+                  <b-form-group
+                    label="EMAIL"
+                    label-for="input-3"
+                    class="engravers label"
+                  >
+                    <b-form-input id="input-3" trim class="footer-input"></b-form-input>
+                  </b-form-group>   
 
-                  </b-container>
+                  <b-form-group
+                    label="SERVICIO"
+                    label-for="input-4"
+                    class="engravers label"
+                  >
+                    <b-form-input id="input-4" trim class="footer-input"></b-form-input>
+                  </b-form-group>  
+
+                  <b-form-group
+                    label="CONSULTA"
+                    label-for="input-4"
+                    class="engravers label"
+                  >
+                    <b-form-textarea
+                    id="input-5"
+                    placeholder="Su consulta"
+                    rows="3"
+                    max-rows="6"
+                    class="footer-input"
+                    ></b-form-textarea>
+                  </b-form-group>
+
+                  <b-button type="submit" block class="footer-input">Enviar</b-button> 
+                  </b-col>
+                </b-row>                  
+                
+          </b-container>
         </v-img>	
 
 
@@ -529,6 +533,7 @@ header .item .cover .header-content h4 {
 .footer-input {
   background-color:rgba(0,0,0,0.2) !important;
   border: 1px solid rgb(111, 66, 45) !important;
+  color: #ffffff;
 }
 
 .label {
