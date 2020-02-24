125f1f00455c7296014d9e865ec5a36f3b0b6380
Pablo Bozzolo <boctulus@gmail.com>
2020-02-24 12:17:31 -0300

    .

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="min-height:100vh">
       <h1>Blog</h1>
   </div>
 </template>
--- pages/servicios.vue
+++ pages/servicios.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="min-height:100vh">
     <h1>Servicios</h1>
   </div>
 </template>
