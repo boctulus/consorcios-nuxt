5936a6104d349363d2b273231e6b50efaab9b8a8
Pablo Bozzolo <boctulus@gmail.com>
2020-03-07 11:51:17 -0300

    .

--- pages/dashboard/posts.vue
+++ pages/dashboard/posts.vue
@@ -1,56 +1,221 @@
 <template>
-  <v-app id="inspire">
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        class="elevation-1"
-      >
-        <template v-slot:items="props">
-          <td>{{ props.item.name }}</td>
-          <td >{{ props.item.calories }}</td>
-          <td>{{ props.item.fat }}</td>
-          <td><v-btn>Created Button</v-btn></td>
-        </template>
-      </v-data-table>
-  </v-app>  
+  <div>
+
+    <div style="text-align: right">
+      <button class="btn">New Item</button>
+    </div>
+    
+
+    <v-app id="inspire">
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          class="elevation-1"
+        >
+          <template v-slot:items="props">
+            
+              <td>{{ props.item.name }}</td>
+              <td >{{ props.item.calories }}</td>
+              <td>{{ props.item.fat }}</td>
+              <td>{{ props.item.carbs }}</td>
+              <td>{{ props.item.protein }}</td>
+              <td>      
+                  <v-icon
+                    small
+                    class="mr-2"
+                    @click="editItem(props.item)"
+                  >
+                    edit
+                  </v-icon>
+
+                  <v-icon
+                    small
+                    @click="deleteItem(props.item)"
+                  >
+                    delete
+                  </v-icon>
+              </td>   
+
+              <template>
+                <v-row justify="center">
+                  <v-dialog v-model="dialog" persistent max-width="290">
+                    <v-card>
+                      <v-card-title class="headline">Use Google's location service?</v-card-title>
+                      <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+                        <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-row>
+              </template>
+
+          </template>
+
+        </v-data-table>
+    </v-app> 
+  </div>  
 </template>
 
 <script>
-export default {  
-  layout: 'dashboard',
-  data () {
-    return {
+  export default {
+    layout: 'dashboard',
+    data: () => ({
+      dialog: false,
       headers: [
         {
           text: 'Dessert (100g serving)',
-          align: 'left',
+          align: 'start',
           sortable: false,
-          value: 'name'
+          value: 'name',
         },
         { text: 'Calories', value: 'calories' },
         { text: 'Fat (g)', value: 'fat' },
-        { text: 'Button field', value: 'buttonField' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Actions', value: 'action', sortable: false },
       ],
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+      defaultItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.desserts = [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+          },
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        const index = this.desserts.indexOf(item)
+        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+      },
+
+      close () {
+        this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        } else {
+          this.desserts.push(this.editedItem)
         }
-      ]
-    }
+        this.close()
+      },
+    },
   }
-}
 </script>
 
 
