2401ec7a97ec359a33c9030d1a2156baadf2b2b9
Pablo Bozzolo <boctulus@gmail.com>
2020-04-13 19:51:24 -0300

    .

---
+++ pages/dashboard/bot.vue
@@ -0,0 +1,474 @@
+<template>
+  <div>            
+    <h1 class="mb-5">Alex Bot</h1>
+  
+    <v-layout row justify-center>
+      <v-dialog v-model="delete_confirmation_dialog" persistent max-width="320">
+        <v-card>
+          <v-card-title class="headline">Confirmación de borrado</v-card-title>
+          <v-card-text>Deseas borrar?</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue" style="color:#fff;" @click="close_delete_confirmation_dialog()">Cancelar</v-btn>
+            <v-btn color="red" style="color:#fff;" @click="erase">OK</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+
+ 
+    <v-layout row justify="center">
+      <v-dialog v-model="dialog" persistent   max-width="800px" :style="{ position: 'absolute', elevation: 100, zIndex:6000 }">
+
+        <!-- New button -->
+        <template v-slot:activator="{ on }">
+          <div style="text-align:right; width: 100%; margin-right: 6px; margin-bottom: 6px;">
+            <v-btn color="primary" dark v-on="on" @click="formMode=null">Nuevo</v-btn>
+          </div>  
+        </template>
+
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{ formTitle }}</span>
+            <a href="#" class="cross_close" @click="close">✕</a>
+          </v-card-title>
+          
+          <v-card-text>
+            <v-text-field v-model="editedItem.question" :class="{'disable-events': formMode=='see'}" label="Pregunta"></v-text-field>
+
+            <label aria-hidden="true" class="v-label v-label--active theme--light" style="left: 0px; right: auto; position: absolute; font-size: 12px !important; margin-left: 32px;">Habilitado?</label>
+            <v-checkbox type="checkbox" v-model="editedItem.enabled" :label="editedItem.enabled | boolean" :class="{'disable-events': formMode=='see'}"/>
+          
+            <v-text-field v-model="editedItem.answer" :class="{'disable-events': formMode=='see'}" label="Respuesta"></v-text-field>
+
+            <div class="mb-2">
+              <span style="rgba(87,87,87); font-familly: Roboto, sans-serif; font-size: 16px; ">Tags</span>
+            </div>
+
+            <vue-tags-input
+              v-model="editedItem.keywords"
+              :tags="tags"
+              @tags-changed="newTags => tags = newTags"
+              :class="{'disable-events': formMode=='see'}"
+            />
+
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="red darken-1" text style="color:#fff;" @click="close">Cerrar</v-btn>
+            <v-btn color="blue darken-1" text style="color:#fff;" @click="save" v-if="formMode == 'edit' || formMode == null">Guardar</v-btn>
+          </v-card-actions>
+
+        </v-card>
+      </v-dialog>
+    </v-layout>
+
+    <div style="
+        background: #fff;
+    ">
+
+          <div style="margin-left: 25px; margin-right: 25px; margin-bottom: 10px;">
+            <v-text-field v-model="search"
+              append-icon="search"
+              label="Buscar"
+              single-line
+              hide-details
+            ></v-text-field>
+          </div>
+
+        <v-data-table
+          :headers="headers"
+            :items="regs"
+            :pagination.sync="pagination"
+            :rows-per-page-items="rowsPerPageItems"
+            :total-items="pagination.totalItems"
+            class="elevation-1"
+        >
+            <template  v-slot:items="props">
+                <td>{{ props.item.question }}</td>
+                <td>{{ props.item.answer | firstWords(4) }}</td>
+                <td>{{ props.item.enabled | boolean }}</td>
+                <td align="right">      
+
+                <v-menu bottom left offset-y>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                        dark
+                        icon
+                        v-on="on"
+                    >
+                    <v-icon style="color: #000;">more_vert</v-icon>
+                  </v-btn>
+                  </template>
+
+                  <v-list style="margin-top: 10px;">
+    
+                    <v-list-tile
+                      @click="seeItem(props.item)"
+                    >
+                      <v-icon
+                        small
+                        style="margin-right:0.9em;"
+                        @click="seeItem(props.item)"
+                      >
+                        fa-eye
+                      </v-icon>
+                    </v-list-tile>
+                    
+                    <v-list-tile
+                      @click="editItem(props.item)"
+                    >
+                      <v-icon
+                        small
+                        style="margin-right:0.9em;"
+                        @click="seeItem(props.item)"
+                      >
+                        edit
+                      </v-icon>
+                    </v-list-tile>
+
+                    <v-list-tile
+                      @click="showDeleteDialog(props.item)"
+                    >
+                      <v-icon
+                        small
+                        style="margin-right:0.9em;"
+                        @click="seeItem(props.item)"
+                      >
+                        delete
+                      </v-icon>
+                    </v-list-tile>
+								
+                  </v-list>
+                </v-menu>
+
+                </td>   
+            </template>            
+
+        </v-data-table>
+    </div> 
+  </div>  
+</template>
+
+<script>
+  // http://www.vue-tags-input.com/#/examples/nuxt
+  import VueTagsInput from '@johmun/vue-tags-input';
+ 
+  export default {
+    layout: 'dashboard',
+
+    data: () => ({
+      entity: 'pregunta',
+      tag: '',
+      tags: [],
+      dialog: false,
+      delete_confirmation_dialog: false,
+      formMode: null,
+      index: null,
+      headers: [
+        { text: 'Pregunta', value: 'question' },
+        { text: 'Respuesta', value: 'answer' },
+        { text: 'Habilitado', value: 'enabled' }
+      ],
+      regs: [],
+      editedIndex: -1,
+      editedItem: {
+        question: '',
+        answer: '',
+        keywords: '',
+        enabled: false
+      },
+      defaultItem: {
+        question: '',
+        answer: '',
+        keywords: '',
+        enabled: false
+      },
+      loading: true,
+      rowsPerPageItems: [10,25,100],
+      pagination: {
+        rowsPerPage: 10,
+        descending: false,
+        sortBy: "created_at",
+        page: 1,
+        totalItems: null,
+      },
+      search: '',
+    }),
+
+    computed: {
+      formTitle: function() {
+        switch(this.formMode){
+          case 'see': 
+            return this.capitalize(this.entity);
+            break;
+          case 'edit': 
+            return 'Editar ' + this.capitalize(this.entity);
+            break;  
+          case 'create': 
+            return 'Crear ' + this.capitalize(this.entity);
+            break;   
+          default: 
+             return 'Crear ' + this.capitalize(this.entity);   
+        }
+      }
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+
+      pagination: {
+          handler() {
+              this.fetchData();
+          },
+          deep: true
+      },
+
+      search: {
+          handler() {
+              this.fetchData();
+          },
+          deep: true
+      },
+      
+      /*
+      'editedItem.enabled': function(val, old_val) {
+        console.log('VAL', val);
+      }
+      */
+    },
+
+    mounted () {
+      this.fetchData();
+    },
+
+    filters: {
+      toBoolean: function (value) {
+        value = value.toString()
+        return value === 0 ? 'true' : 'false';
+      },
+
+      boolean: function (value) {
+        value = value.toString()
+        return (value === 'true' || value === '1') ? 'si' : 'no';
+      },
+
+      stripHtml: function (html)
+      {
+        var tmp = document.createElement("DIV");
+        tmp.innerHTML = html;
+        return tmp.textContent || tmp.innerText || "";
+      },
+
+      firstWords: function (str, n) {
+        const out = str.split(' ').slice(0,n).join(' ');
+
+        return out.length < str.length ? out + ' ...' : out;
+      }
+    },
+
+    methods: {
+      capitalize (s) {
+        if (typeof s !== 'string') return ''
+        return s.charAt(0).toUpperCase() + s.slice(1)
+      },
+
+      fetchData () {
+         return new Promise((resolve, reject) => {
+                const { sortBy, descending, page, rowsPerPage } = this.pagination;
+                let search = this.search.trim().toLowerCase();
+
+                this.$axios.get('/ai_questions' + 
+                  `?pageSize=${rowsPerPage}` +
+                  `&page=${page}` +
+                  `&orderBy[${sortBy}]=` + (descending ? 'ASC' : 'DESC') +
+                  `&keywords[contains]=${search}` , 
+                { 
+                  headers: {
+                    'Authorization': `Bearer ${this.$store.state.auth.authUser.accessToken}`
+                  }
+                })
+                .then(response => {
+                    let items = response.data.data;
+                    const totalItems = response.data.paginator.total;
+                    
+                    if (search) {
+                        items = items.filter(item => {
+                            return Object.values(item)
+                                .join(",")
+                                .toLowerCase()
+                                .includes(search);
+                        });
+                    }
+                    
+                    this.regs = items;
+                    this.pagination.totalItems = totalItems;
+                    this.loading = false;
+                    resolve();
+                }).catch((error) => {
+                    //const response = error.response;
+                    console.log(error);
+                    console.log(error.response);
+                    //reject('Error');
+                });
+         });   
+      },       
+
+      seeItem (item) {
+        this.editedIndex = this.regs.indexOf(item);
+        this.editedItem = Object.assign({}, item);
+        this.editedItem.enabled = (this.editedItem.enabled == 1) ? true : false;
+        this.formMode = 'see';
+        this.dialog = true;
+        this.slug_editable = false; 
+      },
+
+      editItem (item) {
+        this.editedIndex = this.regs.indexOf(item);
+        this.editedItem = Object.assign({}, item);
+        this.editedItem.enabled = (this.editedItem.enabled == 1) ? true : false;
+        this.formMode = 'edit';
+        this.dialog = true;
+        this.slug_editable = false; 
+      },
+
+      showDialog(){
+        this.formMode = 'create';
+        this.dialog = true;
+        this.slug_editable = true; 
+      },
+
+      showDeleteDialog (item) {
+        this.index = this.regs.indexOf(item);
+        this.dialog = false;
+
+        setTimeout(() => { this.delete_confirmation_dialog = true; }, 0);
+      },
+
+      close_delete_confirmation_dialog() {
+        this.delete_confirmation_dialog = false; 
+        this.dialog = false;
+      },
+
+
+      erase () {
+        this.delete_confirmation_dialog = false; 
+        this.formMode = null;
+        
+        const id = this.regs[this.index].id;
+        //console.log('[ DELETE ] ID ==', id);
+        
+        this.$axios.request({
+            url: `/ai_questions/${id}`,  
+            method: 'delete',
+            headers: {
+                'Authorization': `Bearer ${this.$store.state.auth.authUser.accessToken}`
+            }
+        }).then( res => {
+          this.regs.splice(this.index, 1);
+          this.pagination.totalItems--;
+          console.log(res);
+
+        }).catch((error) => {
+            console.log('[ DELETE ] ERROR', error);
+        });
+      },
+
+      close () {
+        this.dialog = false;
+        this.formMode = null;
+        this.slug_editable = true;
+
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem);
+          this.editedIndex = -1;
+        }, 300)
+      },
+
+      save () {
+        this.editedItem.enabled = this.editedItem.enabled ? 1 : 0;
+
+        if (this.editedIndex > -1) {
+          //console.log(this.regs[this.editedIndex]); ////
+          const id = this.regs[this.editedIndex].id;
+
+          this.$axios.request({
+            url: `/ai_questions/${id}`,  
+            method: 'patch',
+            headers: {
+                'Authorization': `Bearer ${this.$store.state.auth.authUser.accessToken}`
+            },
+            data: this.editedItem
+          }).then( ({ data }) => {
+          
+            Object.assign(this.regs[this.editedIndex], this.editedItem);
+            // console.log(data);
+
+            this.close();
+            this.formMode = null;
+          }).catch((error) => {
+              console.log('ERROR', error);
+          });
+
+        } else {
+         
+          this.$axios.request({
+            url: `/ai_questions`,  
+            method: 'post',
+            headers: {
+                'Authorization': `Bearer ${this.$store.state.auth.authUser.accessToken}`
+            },
+            data: this.editedItem
+          }).then( ({ data }) => {
+
+            const uid = data.data.id;
+
+            this.editedItem.id = uid;       
+            this.regs.push(this.editedItem);  
+            this.pagination.totalItems++;
+
+            this.close();
+            this.formMode = null;
+          }).catch((error) => {
+              const response = error.response;
+              //console.log('Error', error);
+              console.log(response.data.error);
+              console.log(response.data.error_detail);
+              //this.error = response.data.error;
+          });
+
+        }
+       
+      },
+
+      
+    },
+
+    components: {
+      VueTagsInput
+    }
+  }
+</script>
+
+
+<style scoped>
+  .cross_close {
+    position: absolute; 
+    right: 10px; 
+    font-size: 1.5rem;
+    color: #ccc;
+    cursor: pointer !important;
+  }
+
+  .cross_close:hover {
+    text-decoration: none;
+    color: #000;
+  }
+
+  .ti-input, .ti-tags {
+    width: 100% !important;
+  }
+</style>
--- pages/test.vue
+++ pages/test.vue
@@ -1,214 +1,50 @@
 <template>
-  <div style="background-color: #3C4043; min-height:100vh;">
-    
-    <div class="logo_wrapper">
-      <div class="logo_container">
-          <n-link to="/home">
-            <v-img src="/elgrove_nueva_tipo.png" lazy-src class="logo" />
-          </n-link>
-      </div>  
-    </div>  
-
-    <div class="container" id="blog_cont">
-        <h1 style="color: #fff;" class="engravers text-mobile mb-5">Blog</h1>
-
-        <b-row v-if="regs.length !=0">
-
-          <b-col sm="12" md="6" class="col_link">
-              <b-row v-for="(post, ix) in regs" v-bind:key="`'row-par-' + ${ix}`" class="area_link" @mouseover="hover = 'par-' + ix"
-    @mouseleave="hover = null">
-                  <span>SLUG: {{ encodeURI(post.slug) }}</span>
-                  <a class="link post_link" :href="`/xxx/${encodeURI(post.slug)}`" > 
-                    <li class="link_wrapper">  
-                        <v-icon class="plus_link" :class="{ color2 : hover == 'par-' + ix}">fa-plus-circle</v-icon>                    
-                        <span href="#" class="link text_link">{{ post.title }}</span>
-                    </li>
-                  </a>
-              </b-row>
-          </b-col>
-
-          
-        </b-row>
-
-    
-        <template v-else>
-          <span style="font-size: 2em;" v-if="!loading">No hay posts en el blog</span>
-        </template>
-   
-    </div>    
-
-
+  <div>
+    <vue-tags-input
+      v-model="tag"
+      :tags="tags"
+      @tags-changed="newTags => tags = newTags"
+    />
   </div>
 </template>
 
 <script>
-import getData from '@/api/posts.js';
+// http://www.vue-tags-input.com/#/examples/nuxt
+import VueTagsInput from '@johmun/vue-tags-input';
 
-export default {  
-  layout: 'home',
-
-  data: () => ({
-    entity: 'post',
-    regs: [],
-    hover: null,
-    loading: true,
-    rowsPerPageItems: [10,25,100],
-    pagination: {
-      rowsPerPage: 50,
-      descending: false,
-      page: 1,
-      totalItems: null,
-      sortBy: "created_at",   
-    },
-    search: '',
-  }),
-  
-
-  computed: {
-    postsPares: function() {
-      //console.log(this.regs);
-
-      return this.regs.data.filter((p, ix) => {
-        return ix % 2 == 0;
-      })
-    },
-
-    postsImpares: function() {
-      return this.regs.filter((p, ix) => {
-        return ix % 2 != 0;
-      })
-    }
-
+export default {
+  components: {
+    VueTagsInput,
+  },
+  data() {
+    return {
+      tag: '',
+      tags: [],
+    };
   },
 
-  async asyncData(context) {
-    const rowsPerPage = 50;
-    const sortBy = "created_at";
-    const descending = false;
-    const page = 1;
-    
-    let response = await context.app.$axios.get('/posts?enabled=1&slug[neq]=home' + 
-      `&pageSize=${rowsPerPage}` +
-      `&page=${page}` +
-      `&orderBy[${sortBy}]=` + (descending ? 'ASC' : 'DESC'), 
-    { 
-      headers: {
-        // 'Authorization': `Bearer ${this.$store.state.auth.authUser.accessToken}`
-      }
+  watch: {
+    tags: function(arr) {
+      let keywords = [];
+      
+      arr.forEach(e => {
+        if (e.tiClasses[0] == 'ti-valid')
+          keywords.push(e.text);
+      });
+
+      console.log(keywords);
+    }
+  },
+
+  mounted() {
+    const keywords = ["peras", "manzanas", "uvas"];
+
+    keywords.forEach(keyword => {
+      this.tags.push({"text": keyword,"tiClasses": ["ti-valid"]})
     });
 
-    return { regs: response.data } 
-  },
+  }
 
-  created() {
-    //console.log(this.regs.data.slice(0,3));
 
-    this.regs = getData();
-  },
-
-  methods: {
-
-  }    
-}
+};
 </script>
-
-
-
-<style scoped>
-:root {
-    --color1: #D0A28C ;
-    --color2: #ccc;
-}
-
-.col_link {
-  padding-left: 5px;
-  padding-right: 5px; 
-}
-
-.area_link {
-  background-color: #27292B; 
-  margin-bottom: 10px; 
-  margin-left: 0.5vw; 
-  margin-right: 0.5vw;
-  height: 80px;
-}
-
-.area_link:hover {
-  background-color: #4E8CFF !important;
-}
-
-.color2 {
-  color: #ccc !important;
-}
-
-.post_link {
-  width: 100% !important;
-}
-
-.link_wrapper {
-  text-align: left; 
-  list-style: none; 
-  margin: 1.5rem;
-}
-
-.plus_link {
-  color: #D0A28C; /* var(--color1); */
-}
-
-.text_link {
-  text-transform: uppercase; 
-  color: #ccc;
-  margin-left: 0.5rem;
-}
-
-
-#blog_cont {
-  text-align: center;
-  color: #ccc;
-}
-
-.logo_container {
-    position: relative;
-    top: 3.5vh;
-    left: 3%; 
-} 
-
-/* texto pequeño en movil*/
-@media only screen and (max-width: 800px){
-    .text-mobile{
-        font-size: 3em !important;
-        /* text-align: left; */
-    }    
-}
-
-@media screen and (max-width: 575px) {
-  h1 {
-    font-size: 1.8em !important;
-  }
-}
-
-@media screen and (min-width: 576px) and (max-width: 767px) {
-  h1 {
-    font-size: 2em !important;
-  }
-}
-
-@media screen and (min-width: 768px) and (max-width: 991px){
-  h1 {
-    font-size: 3em !important;
-  }
-}
-
-@media screen and (min-width: 992px) and (max-width: 1199px){
-  h1 {
-    font-size: 4em !important;
-  }
-}
-
-@media screen and (min-width: 1200px){
-  h1 {
-    font-size: 5em !important;
-  }
-
-}
-</style>
